447164be8d2eba483f4f105e614966f853abe7d2
Zemar Benitez <zemarbenitez@Interapts-MacBook-Pro-103.local>
2018-01-29 14:46:20 -0500

    Finished assesment

---
+++ basic assesments/main.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+
+
+//1. Fix this block of code!
+var number1: Float = 2
+var number2: Float = 2
+var sum = number1 + number2
+// didn't say how to fix the "block of code" so there my way. Found a loop hole.
+
+//2. Create a constant called answer. Answer should be equal to a plus b minus c modulo 2
+var a = 2
+var b = 3
+var c = 4
+
+var str: Int = a + b - c % 2
+
+//3. Create two variables called firstName and lastName. Create a third variable called fullname that is your first name and last name combined using string interpolation.
+
+var firstname = "Zemar"
+var lastname = "Benitez"
+var fullname = ("\(firstname) + \(lastname)")
+
+
+
+
+//4. Using a comment list 2 rules for creating variables.
+
+// You can't add or subtract any Variable if there not the same for of variable.
+// also make sure the variable type fits the use for them.
+
+
+
+//5. Using a for loop and an if statment print out every even number from 1 to 100
+
+
+for number in 1...100 {
+    
+    if number%2 == 0  {
+        print(number)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
